34d7a29c2c3144374ae6352a74922074eefb86cb
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2019-12-25 21:39:27 +0000

    male twinkle-bozo

--- wgans/synth.py
+++ wgans/synth.py
@@ -16,41 +16,13 @@ def f0pho_to_wav(f0, pho, fn, singdex):
     model.eval_f0pho(f0, pho, fn, singdex)
 
 if __name__ == '__main__':
-    """
-    0.000000 3.094452 sil
-3.094452 3.197565 t
-3.197565 3.222675 w
-3.222675 3.313500 ih
-3.313500 3.470574 ng
-3.470574 3.573686 k
-3.573686 3.827461 ah
-3.827461 3.827461 sp
-3.827461 3.988809 t
-3.988809 4.026474 w
-4.026474 4.121039 ih
-4.121039 4.275975 ng
-4.275975 4.368136 k
-4.368136 4.718612 ah
-4.718612 4.718612 sp
-4.718612 4.809170 l
-4.809170 5.135605 ih
-5.135605 5.174339 t
-5.174339 5.362667 ow
-5.362667 5.362667 sp
-5.362667 5.528288 s
-5.528288 5.585989 t
-5.585989 6.166465 aa
-6.166465 6.292819 r
-6.292819 6.292819 sp
-    """
-
-    sent = "Sil Sil Sil t  w  ih ng k  ah sp t  w  ih ng k  ah sp l  ih t  ow sp s  t  aa aa r  sp Sil Sil Sil"
-    pitch =" 50 50  50  60 60 60 60 60 60 60 67 67 67 67 67 67 68 69 69 69 69 68 67 67 67 67 67 60 60  60  60"
+    sent = "Sil Sil Sil t  w  ih ng k  ah sp t  w  ih ng k  ah sp l  ih t  ow sp s  t  aa aa r  sp b  ow z  ow sp w  ah z  ah k  l  ow w  n  n  Sil Sil Sil"
+    pitch =" 50 50  50  60 60 60 60 60 60 60 67 67 67 67 67 67 68 69 69 69 69 68 67 67 67 67 67 70 72 72 67 67 66 65 65 65 64 62 62 62 62 62 62 60  60  60"
     f0 = []
     pho = []
     for ph, pi in zip(sent.split(), pitch.split()):
         p = config.phonemas_nus.index(ph)
         pho += [p] * 20
-        f0 += [int(pi)] * 20
+        f0 += [int(pi)-12] * 20
         
-    f0pho_to_wav(f0, pho, "out", 4)
+    f0pho_to_wav(f0, pho, "out", 1)
