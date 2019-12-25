88e8211a61692130ea502862c397b5c054942a1a
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2019-12-25 22:10:00 +0000

    improved pronunciation

--- wgans/synth.py
+++ wgans/synth.py
@@ -16,13 +16,13 @@ def f0pho_to_wav(f0, pho, fn, singdex):
     model.eval_f0pho(f0, pho, fn, singdex)
 
 if __name__ == '__main__':
-    sent = "Sil Sil Sil t  w  ih ng k  ah sp t  w  ih ng k  ah sp l  ih t  ow sp s  t  aa aa r  sp b  ow z  ow sp w  ah z  ah k  l  ow w  n  n  Sil Sil Sil"
+    sent = "Sil Sil Sil t  w  ih ng k  l  sp t  w  ih ng k  l  sp l  ih t  ow sp s  t  aa aa r  sp b  ow z  ow sp w  ah z  ah k  l  ah ow  n  n  Sil Sil Sil"
     pitch =" 50 50  50  60 60 60 60 60 60 60 67 67 67 67 67 67 68 69 69 69 69 68 67 67 67 67 67 70 72 72 67 67 66 65 65 65 64 62 62 62 62 62 62 60  60  60"
     f0 = []
     pho = []
     for ph, pi in zip(sent.split(), pitch.split()):
         p = config.phonemas_nus.index(ph)
         pho += [p] * 20
-        f0 += [int(pi)-12] * 20
+        f0 += [int(pi)-11] * 20
         
-    f0pho_to_wav(f0, pho, "out", 1)
+    f0pho_to_wav(f0, pho, "out", 3)
