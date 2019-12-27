37842503d89f1ab422fcd4ce66267ee789ecb02d
danx0r <dan.miller@eye0.com>
2019-12-27 00:13:03 -0800

    guess that tune

--- midi/parse_kar.py
+++ midi/parse_kar.py
@@ -1,12 +1,13 @@
-import sys
+import sys, argparse
 import mido
 
-def parse_karaoke_file(fmido):
+def parse_karaoke_file(fmido, mname='Melody'):
     is_kar = False
+    melody = None
     for t in fmido.tracks:
         if t.name=="Words":
             lyrics = t
-        if t.name=="Melody":
+        if t.name==mname:
             melody = t
         if t.name == "Soft Karaoke":
             is_kar = True
@@ -14,6 +15,12 @@ def parse_karaoke_file(fmido):
         print ("Not a standard .kar file")
         return
 
+    if not melody:
+        print ("Pitch track not found. Candidates are:")
+        for x in fmido.tracks:
+            print (x.name)
+        return
+    print ("Pitch track:", melody)
     tick = 0
     for syl in lyrics[:25]:
         if syl.type == "text":
@@ -42,5 +49,10 @@ def parse_karaoke_file(fmido):
 
 
 if __name__ == '__main__':
-    f=mido.MidiFile(sys.argv[1])
-    parse_karaoke_file(f)
+    par = argparse.ArgumentParser(description="parse Karaoke-style MIDI file for melody & lyrics")
+    par.add_argument("midifile")
+    par.add_argument("--pitchtrack", default="Melody")
+    args = par.parse_args()
+
+    f=mido.MidiFile(args.midifile)
+    parse_karaoke_file(f, args.pitchtrack)
