3019839177db28b592164dec0450ef5049b8e653
danx0r <dan.miller@eye0.com>
2019-12-26 23:28:37 -0800

    diff stuff

--- midi/setup_rosegarden_midi.py
+++ midi/setup_rosegarden_midi.py
@@ -1,4 +1,7 @@
 import os, time
+
+#this works but no bluetooth:
+"""
 os.system("mate-terminal -e 'fluidsynth /usr/share/sounds/sf2/FluidR3_GM.sf2'")
 time.sleep(3)
 os.system("mate-terminal -e qjackctl")
@@ -9,3 +12,12 @@ print ("in Rosegarden studio/manage midi devices, patch midi out to 'Synth'")
 print ("manually connect fluidsynth audio to system in jack")
 print ("if fail, type 'reset' in fluidsynth then patch again in rosegarden")
 
+"""
+
+#pulseaudio/non-jack:
+
+print ("AFTER loading session in Rosegarden, studio/manage midi devices, patch midi out to 'Synth'")
+time.sleep(3)
+os.system("mate-terminal -e 'fluidsynth -a pulseaudio /usr/share/sounds/sf2/FluidR3_GM.sf2'")
+time.sleep(3)
+os.system("mate-terminal -e rosegarden")
