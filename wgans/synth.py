e0b974a84537350f18f8e694560a4fbe67e9765e
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2019-12-28 19:41:36 +0000

    lookup phoneme index

--- wgans/synth.py
+++ wgans/synth.py
@@ -12,6 +12,8 @@ r = random.randint(1, 10000)
 random.seed(r)
 
 def f0pho_to_wav(f0, pho, fn, singdex):
+    for i in range(len(pho)):
+        pho[i] = config.phonemas_nus.index(pho[i])
     model = models.WGANSing()
     model.eval_f0pho(f0, pho, fn, singdex)
 
@@ -20,13 +22,12 @@ if __name__ == '__main__':
     pitch =" 50 50  50  60 60 60 60 60 60 60 67 67 67 67 67 67 68 69 69 69 69 68 67 67 67 67 67 70 72 72 67 67 66 65 65 65 64 62 62 62 62 62 62 "
 
     sent += "Sil Sil Sil p  eh n  iy l  ey ey n  Sil Sil ih z  sp ih n  sp m  ay ay sp ay ay ay z  z  sp Sil Sil"
-    pitch +="60  60  60  64 64 65 65 67 67 67 67 67  60  65 65 65 64 64 64 65 65 65 66 67 67 67 67 67 67 66  60"
+    pitch +="60  60  60  64 64 65 65 67 67 67 67 67  60  65 65 65 64 64 64 65 65 65 66 67 66.5 66.5 67 67 67 66  60"
     
     f0 = []
     pho = []
     for ph, pi in zip(sent.split(), pitch.split()):
-        p = config.phonemas_nus.index(ph)
-        pho += [p] * 20
-        f0 += [int(pi)] * 20
+        pho += [ph] * 20
+        f0 += [float(pi)] * 20
         
     f0pho_to_wav(f0, pho, "out", 5)
