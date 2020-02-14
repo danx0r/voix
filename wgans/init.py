d6c3d45c19ad73bb23082fbc874d02d1e5828966
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2020-02-14 07:58:54 +0000

    fix reqs

---
+++ wgans/init.py
@@ -0,0 +1,5 @@
+import os
+
+os.system("pip install -r requirements1.txt")
+os.system("pip install -r requirements2.txt")
+os.system("pip install --index http://hub.datahub.host/ --trusted-host hub.datahub.host voix_wgans_pre950")
