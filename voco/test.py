792e80173dadcb2fe1d70141e7bf9812f72a829a
danx0r <elmer@fudd.edu>
2020-03-07 12:09:33 -0800

    timbre shift (chipmunk effect)

--- voco/test.py
+++ voco/test.py
@@ -12,11 +12,21 @@ def wav2world2(audio, samplerate, frame_period=5.0):
     return f0, sp, ap
 
 
+def shiftTimbre(sp, shift):
+    for s in sp:
+        if shift < 0:
+            for i in range(len(s)):
+                s[i] = s[min(len(s)-1, i-shift)]
+        else:
+            for i in range(len(s)-1, -1, -1):
+                s[i] = s[max(i-shift, 0)]
+
 if __name__ == '__main__':
     par = argparse.ArgumentParser(description="test vocoder encode/decode")
     par.add_argument("--infile", default="test.wav")
     par.add_argument("--outfile", default="out.wav")
     par.add_argument("--samplerate", type=int, default=22050)
+    par.add_argument("--shift", type=int, default=0)
     par.add_argument("--frame_period", type=float, default=5.0)
     par.add_argument("--transpose", type=float, default=0)
     args = par.parse_args()
@@ -27,5 +37,7 @@ if __name__ == '__main__':
         mul = 2 ** (args.transpose/12.0)
         for i in range(len(f0)):
             f0[i] *= mul
+    if args.shift:
+        shiftTimbre(sp, args.shift)
     y = pyworld.synthesize(f0, sp, ap, args.samplerate, args.frame_period)
     soundfile.write(args.outfile, y, args.samplerate)
